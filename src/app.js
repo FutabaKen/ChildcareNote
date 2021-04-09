89a410696832a0773f2d9aab1d57b6e5eb74bc0b
futabaken <who_is_this_king_of_glory@yahoo.co.jp>
2021-04-09 11:11:58 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// MySQLを使うためのコードを貼り付けてください
+const mysql = require('mysql');
+
+const app = express();
+
+app.use(express.static('public'));
+// 定数connectionを定義して接続情報の書かれたコードを代入してください
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'progate',
+  password: 'password',
+  database: 'list_app'
+});
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/noteList', (req, res) => {
+  res.render('noteList.ejs');
+});
+
+app.listen(3000);
