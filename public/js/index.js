8e5353ba678d963c1aa2cc10ba49382a008bcd2e
futabaken <who_is_this_king_of_glory@yahoo.co.jp>
2021-07-28 10:56:37 +0900

    vueが動くようになった npmスクリプト実装につき、ディレクトリ構成を見直し

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+var title__header = new Vue({
+    el: '#title__header',
+    data: {
+      title__header: '乳幼児れんらくノート'
+    }
+  })
--- src/public/js/index.js
+++
@@ -1,6 +0,0 @@
-var app = new Vue({
-    el: '#title__header',
-    data: {
-      message: '乳幼児れんらくノート'
-    }
-  })
