474a85727a914ecd8ecf76ff69e3055c410b2ff1
futabaken <who_is_this_king_of_glory@yahoo.co.jp>
2021-07-28 15:45:10 +0900

    ｓｃｓｓをパーシャル化 ハンバーガーメニュー作成中

--- public/js/include.js
+++ public/js/include.js
@@ -1,4 +1,14 @@
+// ajax 通信
 $(function () {
-    $.ajaxSetup({ cache: false });
-    $('header').load('/header');
+    $.ajax('/header', {
+        // options
+        cache: false,
+        async: true, // 非同期処理
+        dataType: 'html',
+    }).done(function(data) {
+        var out_html = $.parseHTML(data);
+        $('header').empty().append(out_html);
+    }).fail(function() {
+        console.error('ajax通信に失敗しました');
+    });
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,29 @@
+'use strict';
+
 var title__header = new Vue({
     el: '#title__header',
     data: {
-      title__header: '乳幼児れんらくノート'
+        title__header: '乳幼児れんらくノート'
     }
-  })
+})
+
+function header() {
+
+    const body = document.body;
+    const btnHamburger = document.getElementById('btnNavbarToggle');
+    const navbarNav2 = document.getElementById('navbarNav2');
+    const navlinks = document.querySelectorAll('#navbarNav2 a')
+
+    btnHamburger.addEventListener('click', function () {
+        navbarNav2.classList.toggle('navbar__collapse--open');
+        navbarNav2.classList.toggle('navbar__collapse');
+        console.log(navbarNav2);
+        // console.table(navlinks);
+        navlinks.forEach(navlink => {
+            console.log(navlink);
+            navlink.classList.toggle('nav__link--open');
+            navlink.classList.toggle('nav__link');
+        });
+    });
+}
+header();
